4c38b7af046a6b038252bc5f3cc024f91bccd92a
ansar <ansarsaudagar40@gmail.com>
2024-11-03 21:17:51 +0530

    migration file and resources created for product_category

---
+++ src/migrations/20241103154008-create-product-category.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("product_category", {
+      id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      image: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.NOW,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("product_category");
+  },
+};
